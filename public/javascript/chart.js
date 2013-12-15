05825e15c9b6e92f258cad1c187118a788405fff
Aaron Klaassen <aaron@outerspacehero.com>
2013-12-15 14:09:52 -0600

    Add current temp column to chart.

--- public/javascript/chart.js
+++ public/javascript/chart.js
@@ -3,13 +3,14 @@ google.setOnLoadCallback(loadData);
 
 
 function drawChart(data_array) {
-  data_array.unshift(["Date", "Normal High", "Normal Low", "Actual High", "Actual Low"]);
+  data_array.unshift(["Date", "Normal High", "Normal Low", "Actual High", "Actual Low", "Current"]);
   var data = google.visualization.arrayToDataTable(data_array);
 
   var options = {
     colors: ["#0000FF", "#0000BB", "#FF0000", "#BB0000"],
     hAxis: { showTextEvery: 7 },
-    vAxis: { title: "Temp (°C)" }
+    vAxis: { title: "Temp (°C)" },
+    series: { 4: { pointSize: 10, color: "#FFAA00" } }
   };
 
   var chart = new google.visualization.LineChart(document.getElementById('temp-chart'));
